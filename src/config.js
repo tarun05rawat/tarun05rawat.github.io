9839714cf52ff61a2e077e09d94d83b73bde009a
Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>
2024-10-03 19:14:12 -0400

    changed social links

--- src/config.js
+++ src/config.js
@@ -4,23 +4,16 @@ module.exports = {
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
-    },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://github.com/tarun05rawat',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/tarun05rawat/',
     },
     {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      name: 'Devpost',
+      url:
+        'https://devpost.com/tarun05rawat?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav',
     },
   ],
 
