92455dd7ca236bc1801009237fd2d2c4f1f5898f
Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>
2025-09-15 02:24:10 -0400

    updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Icon } from '@components/icons';
@@ -67,65 +67,29 @@ const StyledCredit = styled.div`
   }
 `;
 
-const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
+const Footer = () => (
+  <StyledFooter>
+    <StyledSocialLinks>
+      <ul>
+        {socialMedia &&
+          socialMedia.map(({ name, url }, i) => (
+            <li key={i}>
+              <a href={url} aria-label={name}>
+                <Icon name={name} />
+              </a>
+            </li>
+          ))}
+      </ul>
+    </StyledSocialLinks>
 
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      return;
-    }
-    fetch('https://api.github.com/repos/bchiang7/v4')
-      .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch(e => console.error(e));
-  }, []);
-
-  return (
-    <StyledFooter>
-      <StyledSocialLinks>
-        <ul>
-          {socialMedia &&
-            socialMedia.map(({ name, url }, i) => (
-              <li key={i}>
-                <a href={url} aria-label={name}>
-                  <Icon name={name} />
-                </a>
-              </li>
-            ))}
-        </ul>
-      </StyledSocialLinks>
-
-      <StyledCredit tabindex="-1">
-        {/* <div>
-          Designed and Built by Tarun Rawat, using a template by
-          <a href="https://github.com/bchiang7/v4">Brittany Chiang</a>
-        </div> */}
-
-        {githubInfo.stars && githubInfo.forks && (
-          <div className="github-stats">
-            <span>
-              <Icon name="Star" />
-              <span>{githubInfo.stars.toLocaleString()}</span>
-            </span>
-            <span>
-              <Icon name="Fork" />
-              <span>{githubInfo.forks.toLocaleString()}</span>
-            </span>
-          </div>
-        )}
-      </StyledCredit>
-    </StyledFooter>
-  );
-};
+    <StyledCredit tabindex="-1">
+      {/* <div>
+        Designed and Built by Tarun Rawat, using a template by
+        <a href="https://github.com/bchiang7/v4">Brittany Chiang</a>
+      </div> */}
+    </StyledCredit>
+  </StyledFooter>
+);
 
 Footer.propTypes = {
   githubInfo: PropTypes.object,
