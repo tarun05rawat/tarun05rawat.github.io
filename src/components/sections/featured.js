1c013b6430c4df63acbf9170968f703b89b86b47
Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>
2026-02-20 01:44:27 -0500

    fixed title overlap

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -126,6 +126,8 @@ const StyledProject = styled.li`
   .project-title {
     color: var(--lightest-slate);
     font-size: clamp(24px, 5vw, 28px);
+    max-width: 480px;
+    line-height: 1.2;
 
     @media (min-width: 768px) {
       margin: 0 0 20px;
